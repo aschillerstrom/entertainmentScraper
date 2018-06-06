590bb594a4772625edc6bbfeaceecb17657581e7
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-06-05 21:31:28 -0500

    basic server code

---
+++ server.js
@@ -0,0 +1,59 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require for scraping
+var request = require("request");
+var cheerio = require("cheerio");
+//require handlebars for style
+var exphbs = require('express-handlebars');
+//require for debugging
+var logger = require('morgan'); 
+//require mongoose
+var mongoose = require('mongoose');
+
+// Initialize Express
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+//static content
+app.use(express.static(process.cwd() + '/public'));
+
+//express handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// database configuration with mongoose???? NEEDS WORK
+
+  mongoose.connect('mongodb://localhost/news-scraper');
+
+var db = mongoose.connection;
+
+// display errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// show a success message when connected
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+// imports the notes and articles models
+var Comment = require('./models/note.js');
+var Article = require('./models/article.js');
+// ---------------------------------------------------------------------------------------------------------------
+
+
+// import controller
+var router = require('./controllers/controller.js');
+app.use('/', router);
+
+
+// launch app
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Running on port: ' + port);
+});
