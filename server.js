d1827f13aa6fb77c7dc0539a3577b5fa2ef18e2c
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-06-08 20:58:53 -0500

    routes problems

--- server.js
+++ server.js
@@ -1,75 +1,52 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// Require for scraping
-var request = require("request");
-var cheerio = require("cheerio");
-//require handlebars for style
-var exphbs = require('express-handlebars');
-//require for debugging
-var logger = require('morgan'); 
-//require mongoose
-var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-var bodyParser = require('body-parser');
+// require all models
+var db = require("./models");
 
-// Initialize Express
-var app = express();
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-
-//static content
-app.use(express.static(process.cwd() + '/public'));
-
-//express handlebars
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-// database configuration with mongoose???? NEEDS WORK  IS THIS RIGHT??
-
-mongoose.Promise = Promise;
-if(process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI) 
-    }
-else {
-    mongoose.connect("mongodb://localhost/onionnews")
-    };
-
-var db = mongoose.connection;
-
-// display errors
-db.on('error', function(err) {
-  console.log('Mongoose Error: ', err);
-});
-
-// show a success message when connected
-db.once('open', function() {
-  console.log('Mongoose connection successful.');
-});
-
-// imports the notes and articles models
-require('./models/note.js')(app);
-require('./models/article.js')(app);
-// ---------------------------------------------------------------------------------------------------------------
-
-
-// import controller
-var router = require('./controllers/controller.js');
-app.use('/', router);
-
-
-// launch app
 var PORT = process.env.PORT || 3000;
 
+// Initialize Express
+var app = express();    
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+//Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)}
+else {
+    mongoose.connect("mongodb://localhost/austinnews")
+}
+
+// routes
+require("./controllers/controller.js")(app);
+
+// start the server
 mongoose.connection.on('error', function(err) {
-  console.log("Mongoose Error: " + err);
+    console.log("Mongoose Error: " + err);
 })
 
 mongoose.connection.on('open', function() {
-  console.log("Mongoose connection successful.");
-  app.listen(PORT, function() {
-      console.log("App running on port " + PORT);
-  });
+    console.log("Mongoose connection successful.");
+    app.listen(PORT, function() {
+        console.log("App running on port " + PORT);
+    });
 });
